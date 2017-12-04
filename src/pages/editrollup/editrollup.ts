4c3396503d5566053b13d24adc0bb484d79ad85d
BrucePaterson64 <bruce_paterson@sky.com>
2017-12-04 17:12:31 +0000

    mods

--- src/pages/editrollup/editrollup.ts
+++ src/pages/editrollup/editrollup.ts
@@ -2,21 +2,46 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {DataServiceProvider} from '../../providers/data-service/data-service';
 import {Http, Headers} from '@angular/http';
-
+import { OrderModule } from 'ngx-order-pipe';
+import {Storage} from '@ionic/storage';
 @IonicPage()
 @Component({
   selector: 'page-editrollup',
   templateUrl: 'editrollup.html',
 })
 export class EditrollupPage {
-    course; time; day;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public dataService: DataServiceProvider, public http : Http) {
+    course; time; day; 
+    Club:any; 
+    Time:string; 
+    Day:any;
+    selectedClub; selectedTime; selectedDay;
+   
+  constructor(public navCtrl: NavController, public navParams: NavParams, public dataService: DataServiceProvider, public http : Http, public storage: Storage) {
+  
+  
   this.loadCourses();
   this.loadD();
   this.loadT();
   }
   
-     loadCourses(){
+  ngOnInit() {
+  this.storage.get('club').then((club) => {
+  this.Club = club;
+  this.selectedClub = this.Club;
+    })
+  this.storage.get('day').then((day) => {
+  this.Day = day;
+  this.selectedDay = this.Day;
+    })
+  this.storage.get('time').then((time) => {
+  this.Time = time;
+  this.selectedTime = this.Time;
+    })
+  }
+  
+  
+  loadCourses(){
+    
      this.dataService.loadCourseRU()
       .then(data => {
       this.course = data;
@@ -31,7 +56,7 @@ export class EditrollupPage {
       .map(res => res.json())
     .subscribe(data => {
     this.time = data;
-     console.log(this.time);
+    
   })  
 
 }
